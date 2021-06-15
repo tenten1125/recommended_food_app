4eaf46635cbf4a2a0f07da9b733363071edf608b
yoshito <yoshito5211@gmail.com>
2021-06-15 11:55:12 +0900

    いいね機能用のルーティング・コントローラを作成

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,4 +3,8 @@ class Food < ApplicationRecord
   has_many :likes, dependent: :destroy
   validates :name, presence: true
   mount_uploader :image, ImageUploader
+
+  def liked_by?(user)
+    likes.exists?(user_id: user.id)
+  end
 end
